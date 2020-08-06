547239dcd6fb5dd094f862704f051c300b984113
pragati.hudelabs <pragati@hudelabs.com>
2020-08-06 16:00:17 +0530

    products and offers added

--- products/models.py
+++ products/models.py
@@ -11,3 +11,4 @@ class Offer(models.Model):
     code = models.CharField(max_length=20)
     description = models.CharField(max_length=255)
     discount = models.FloatField()
+
--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
 
 def newpage(request):
     return HttpResponse("This is demo page.")
